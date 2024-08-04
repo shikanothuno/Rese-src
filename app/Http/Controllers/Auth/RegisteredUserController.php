67682e4d09fae37cb48415bd48314aa9706dc510
shikanothuno <ri0066xr@gmail.com>
2024-08-04 11:02:24 +0900

    commit

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -40,6 +40,6 @@ class RegisteredUserController extends Controller
 
         Auth::login($user);
 
-        return redirect(route('shop-list', absolute: false));
+        return redirect(route('verification.notice', absolute: false));
     }
 }
--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,31 +1,35 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
-    </div>
+@extends('layouts/layout-base')
 
-    @if (session('status') == 'verification-link-sent')
-        <div class="mb-4 font-medium text-sm text-green-600">
-            {{ __('A new verification link has been sent to the email address you provided during registration.') }}
+@section('title')
+    メール認証
+@endsection
+
+@section('css')
+    <link rel="stylesheet" href="{{ asset("css/verify-email.css") }}">
+@endsection
+
+@section('content')
+<main>
+    <div class="container">
+        <div class="text">
+            ご登録ありがとうござます。
+            ご登録のメールアドレスに、<br>
+            メールアドレス確認リンクを
+            送付いたしました。<br>
+            確認リンクをクリックし、
+            登録を完了してください。<br>
+            メールが届いてない場合は、
+            下記から再送してください。<br>
         </div>
-    @endif
-
-    <div class="mt-4 flex items-center justify-between">
-        <form method="POST" action="{{ route('verification.send') }}">
+        <form action="POST" action="{{ route("verification.send") }}">
             @csrf
-
-            <div>
-                <x-primary-button>
-                    {{ __('Resend Verification Email') }}
-                </x-primary-button>
-            </div>
-        </form>
-
-        <form method="POST" action="{{ route('logout') }}">
-            @csrf
-
-            <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                {{ __('Log Out') }}
+            <button>
+                メールアドレスリンクの再送
             </button>
         </form>
+
     </div>
-</x-guest-layout>
+
+</main>
+@endsection
+
