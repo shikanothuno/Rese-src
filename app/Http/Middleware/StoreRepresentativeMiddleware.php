992ce6ecaecf70ebb42755e8f696991f4ca81d68
shikanothuno <ri0066xr@gmail.com>
2024-08-16 15:04:23 +0900

    commit

--- app/Http/Middleware/StoreRepresentativeMiddleware.php
+++ app/Http/Middleware/StoreRepresentativeMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
 
 class StoreRepresentativeMiddleware
@@ -15,10 +16,10 @@ class StoreRepresentativeMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        $shop_id = $request->route("shop_id");
+        $shop_id = $request->route("shop");
 
         if(auth()->check() && auth()->user()->is_store_representative &&
-        auth()->user()->shop_id == $shop_id){
+        auth()->user()->shop_id == strval($shop_id)){
             return $next($request);
         }
         return redirect(route("shop-list"));
